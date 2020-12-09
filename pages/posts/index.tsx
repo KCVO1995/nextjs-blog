2ec195597c0f1d6c28f92ed5a054354ddce4bff8
kcvo <943514589@qq.com>
2020-12-09 23:35:07 +0800

    feat: 基本完成分页

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -6,13 +6,15 @@ import Link from 'next/link';
 
 type Props = {
   posts: Post[]
+  count: number
+  page: number
 }
 
 const PostsIndex: NextPage <Props> = (props) => {
-  const {posts} = props
+  const {posts, count, page} = props
   return (
     <div>
-      <h2>文字列表</h2>
+      <h2>文字列表 {count}</h2>
       {
         posts.map(post =>
           <Link key={post.id} href={'/posts/' + post.id}>
@@ -22,18 +24,32 @@ const PostsIndex: NextPage <Props> = (props) => {
           </Link>
         )
       }
+      <footer>
+        <span>第 {page} 页，</span>
+        <span>总 {count} 篇</span>
+        <div>
+          <Link href={'?page=' + (page - 1)}><a>上一页</a></Link>
+          /
+          <Link href={'?page=' + (page + 1)}><a>下一页</a></Link>
+        </div>
+      </footer>
     </div>
   )
 }
 
 export default PostsIndex
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const connection = await getDatabaseConnection()
-  const posts = await connection.manager.find(Post)
+  const page = context.query.page && parseInt(context.query.page.toString()) || 1
+  console.log(page, 'page')
+  const pageSize = context.query.pageSize && parseInt(context.query.pageSize.toString()) || 2
+  const [posts, count] = await connection.manager.findAndCount('Post', {skip: (page - 1) * pageSize, take: pageSize})
   return {
     props: {
       posts: JSON.parse(JSON.stringify(posts)),
+      count,
+      page
     }
   }
 }
